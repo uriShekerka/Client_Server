967df6accdd062e547f10bc12f44d7ce3fafd5e3
uriShekerka <116493322+uriShekerka@users.noreply.github.com>
2023-01-02 10:24:06 +0000

    new

--- server/db/mongoConnect.js
+++ server/db/mongoConnect.js
@@ -4,6 +4,6 @@ require('dotenv').config()
 main().catch(err => console.log(err)); //Error Message if not working the connect
 
 async function main() { //connect Data Base from Mongo Atlas by user & password 
-    await mongoose.connect(`mongodb://127.0.0.1:27018/myapp`); //(feb22 == name DataBase)
+    await mongoose.connect(`mongodb+srv://uri:1234@cluster0.v0gts8j.mongodb.net/`); //(feb22 == name DataBase)
     console.log(`Mongo Atlas connect...`) //print--> Mongo is Working
 }
